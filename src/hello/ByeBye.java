5783d1c90071befa2fae79377e98b8cbada4cee8
haroon-sadiq2 <haroon.sadiq.@tekschool.us>
2022-10-08 22:03:20 -0700

    This is fucking new Folder

---
+++ src/hello/ByeBye.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class ByeBye {
+
+	public static void main(String[] args) {
+		System.out.println("Hello This is new code");
+		System.out.println("Hello This is new code");
+		System.out.println("Hello This is new code");
+		System.out.println("Hello This is new code");
+		System.out.println("Hello This is new code");
+
+	}
+
+}
