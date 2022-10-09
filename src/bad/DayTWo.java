d18368fbba052bcf5bed502f1070845af4724384
haroon-sadiq2 <haroon.sadiq.@tekschool.us>
2022-10-08 21:11:18 -0700

    first practice

---
+++ src/bad/DayTWo.java
@@ -0,0 +1,11 @@
+package bad;
+
+public class DayTWo {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Hello This is new code");
+	}
+
+}
--- src/bad/good.java
+++ src/bad/good.java
@@ -4,6 +4,9 @@ public class good {
 
 	
 	public static void main(String[] args) {
+		
+		
+		
 		System.out.println("Hello this is haron");
 	}
 }
